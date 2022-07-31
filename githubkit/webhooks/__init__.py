7912df045a9427d4b7f418d2aa758ecfc3501472
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2022-07-31 06:32:49 +0000

    :sparkles: add webhook parse dict

--- githubkit/webhooks/__init__.py
+++ githubkit/webhooks/__init__.py
@@ -2,5 +2,7 @@ from .models import *
 from .verify import sign as sign
 from .parse import parse as parse
 from .verify import verify as verify
+from .parse import parse_obj as parse_obj
 from .verify import normalize_payload as normalize_payload
 from .parse import parse_without_name as parse_without_name
+from .parse import parse_obj_without_name as parse_obj_without_name
--- githubkit/webhooks/parse.py
+++ githubkit/webhooks/parse.py
@@ -1,6 +1,6 @@
-from typing import AnyStr
+from typing import Any, Dict, AnyStr
 
-from pydantic import parse_obj_as
+from pydantic import parse_obj_as, parse_raw_as
 
 from githubkit.exception import WebhookTypeNotFound
 
@@ -8,10 +8,20 @@ from .models import WebhookEvent, webhook_events
 
 
 def parse_without_name(payload: AnyStr) -> WebhookEvent:
-    return parse_obj_as(WebhookEvent, payload)
+    return parse_raw_as(WebhookEvent, payload)
 
 
 def parse(name: str, payload: AnyStr) -> WebhookEvent:
+    if name not in webhook_events:
+        raise WebhookTypeNotFound(name)
+    return parse_raw_as(webhook_events[name], payload)
+
+
+def parse_obj_without_name(payload: Dict[str, Any]) -> WebhookEvent:
+    return parse_obj_as(WebhookEvent, payload)
+
+
+def parse_obj(name: str, payload: Dict[str, Any]) -> WebhookEvent:
     if name not in webhook_events:
         raise WebhookTypeNotFound(name)
     return parse_obj_as(webhook_events[name], payload)
