a76f8ef7b11844e6538d54364ccc3ade907776e8
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2024-01-16 17:59:16 +0800

    :memo: update readme

--- githubkit/retry.py
+++ githubkit/retry.py
@@ -7,15 +7,15 @@ from .exception import RequestFailed, GitHubException, RateLimitExceeded
 
 
 class RetryRateLimit:
-    __slots__ = ("max_retry_count",)
+    __slots__ = ("max_retry",)
 
-    def __init__(self, max_retry_count: int = 1) -> None:
-        self.max_retry_count = max_retry_count
+    def __init__(self, max_retry: int = 1) -> None:
+        self.max_retry = max_retry
 
     def __call__(self, exc: GitHubException, retry_count: int) -> RetryOption:
         # retry once for rate limit exceeded
         # https://github.com/octokit/octokit.js/blob/450c662e4f29b63a6dbe7592ba5ef53d5fa01d88/src/octokit.ts#L34-L65
-        if retry_count < self.max_retry_count and isinstance(exc, RateLimitExceeded):
+        if retry_count < self.max_retry and isinstance(exc, RateLimitExceeded):
             logger.warning(
                 f"{exc.__class__.__name__} for request "
                 f"{exc.request.method} {exc.request.url}",
@@ -30,16 +30,16 @@ RETRY_RATE_LIMIT = RetryRateLimit()
 
 
 class RetryServerError:
-    __slots__ = ("max_retry_count",)
+    __slots__ = ("max_retry",)
 
-    def __init__(self, max_retry_count: int = 3) -> None:
-        self.max_retry_count = max_retry_count
+    def __init__(self, max_retry: int = 3) -> None:
+        self.max_retry = max_retry
 
     def __call__(self, exc: GitHubException, retry_count: int) -> RetryOption:
         # retry three times for server error
         # https://github.com/octokit/plugin-retry.js/blob/3b9897a58d8b2c37cfba6a8df78e4619d346098a/src/error-request.ts#L11-L14
         if (
-            retry_count < self.max_retry_count
+            retry_count < self.max_retry
             and isinstance(exc, RequestFailed)
             and exc.response.status_code >= 500
         ):
