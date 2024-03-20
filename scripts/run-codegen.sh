5ac122405e2bfe6a8e3167984e266b889dd058fd
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2024-03-20 14:20:48 +0000

    :wrench: update missing config

--- scripts/run-codegen.sh
+++ scripts/run-codegen.sh
@@ -3,4 +3,4 @@
 # cd to the root of the project
 cd "$(dirname "$0")/.."
 
-python -m codegen && isort . && ruff check --fix --exit-zero . && ruff format .
+python -m codegen && ruff check --fix --exit-zero . && isort . && ruff format .
