dcb9a3dce14fff7bb3f91c53b1a6f677e3a6a6e2
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2024-07-28 03:34:02 +0000

    :sparkles: move graphql to namespace

--- githubkit/graphql/__init__.py
+++ githubkit/graphql/__init__.py
@@ -1,3 +1,4 @@
+from weakref import ref
 from typing import TYPE_CHECKING, Any, Dict, Optional, cast
 
 from githubkit.exception import GraphQLFailed, PrimaryRateLimitExceeded
@@ -11,27 +12,68 @@ if TYPE_CHECKING:
     from githubkit.response import Response
 
 
-def build_graphql_request(
-    query: str, variables: Optional[Dict[str, Any]] = None
-) -> Dict[str, Any]:
-    json: Dict[str, Any] = {"query": query}
-    if variables:
-        json["variables"] = variables
-    return json
+class GraphQLNamespace:
+    def __init__(self, github: "GitHubCore") -> None:
+        self._github_ref = ref(github)
 
+    @property
+    def _github(self) -> "GitHubCore":
+        if g := self._github_ref():
+            return g
+        raise RuntimeError(
+            "GitHub client has already been collected. "
+            "Do not use the namespace after the client has been collected."
+        )
 
-def parse_graphql_response(
-    github: "GitHubCore", response: "Response[GraphQLResponse]"
-) -> Dict[str, Any]:
-    response_data = response.parsed_data
-    if response_data.errors:
-        # check rate limit exceeded
-        # https://docs.github.com/en/graphql/overview/rate-limits-and-node-limits-for-the-graphql-api#exceeding-the-rate-limit
-        # x-ratelimit-remaining may not be 0, ignore it
-        # https://github.com/octokit/plugin-throttling.js/pull/636
-        if any(error.type == "RATE_LIMITED" for error in response_data.errors):
-            raise PrimaryRateLimitExceeded(
-                response, github._extract_retry_after(response)
-            )
-        raise GraphQLFailed(response_data)
-    return cast(Dict[str, Any], response_data.data)
+    @staticmethod
+    def build_graphql_request(
+        query: str, variables: Optional[Dict[str, Any]] = None
+    ) -> Dict[str, Any]:
+        json: Dict[str, Any] = {"query": query}
+        if variables:
+            json["variables"] = variables
+        return json
+
+    def parse_graphql_response(
+        self, response: "Response[GraphQLResponse]"
+    ) -> Dict[str, Any]:
+        response_data = response.parsed_data
+        if response_data.errors:
+            # check rate limit exceeded
+            # https://docs.github.com/en/graphql/overview/rate-limits-and-node-limits-for-the-graphql-api#exceeding-the-rate-limit
+            # x-ratelimit-remaining may not be 0, ignore it
+            # https://github.com/octokit/plugin-throttling.js/pull/636
+            if any(error.type == "RATE_LIMITED" for error in response_data.errors):
+                raise PrimaryRateLimitExceeded(
+                    response, self._github._extract_retry_after(response)
+                )
+            raise GraphQLFailed(response_data)
+        return cast(Dict[str, Any], response_data.data)
+
+    def request(
+        self, query: str, variables: Optional[Dict[str, Any]] = None
+    ) -> Dict[str, Any]:
+        json = self.build_graphql_request(query, variables)
+
+        return self.parse_graphql_response(
+            self._github.request(
+                "POST", "/graphql", json=json, response_model=GraphQLResponse
+            ),
+        )
+
+    async def arequest(
+        self, query: str, variables: Optional[Dict[str, Any]] = None
+    ) -> Dict[str, Any]:
+        json = self.build_graphql_request(query, variables)
+
+        return self.parse_graphql_response(
+            await self._github.arequest(
+                "POST", "/graphql", json=json, response_model=GraphQLResponse
+            ),
+        )
+
+    # backport for calling graphql directly
+    def __call__(
+        self, query: str, variables: Optional[Dict[str, Any]] = None
+    ) -> Dict[str, Any]:
+        return self.request(query, variables)
