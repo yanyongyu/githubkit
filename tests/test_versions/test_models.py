13751cababc87fbbe86dbd9741b82a77977b28fc
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2024-09-16 04:00:28 +0000

    :white_check_mark: add test cases

--- tests/test_versions/test_models.py
+++ tests/test_versions/test_models.py
@@ -1,3 +1,6 @@
+# import some commonly used models to ensure they are available
+
+# SimpleUser
 from githubkit.versions.latest.models import (  # noqa: F401
     SimpleUser as SimpleUserLatest,
 )
@@ -6,4 +9,43 @@ from githubkit.versions.v2022_11_28.models import (  # noqa: F401
 )
 from githubkit.versions.ghec_v2022_11_28.models import (  # noqa: F401
     SimpleUser as SimpleUserGhecV2022_11_28,
+)
+
+# isort: split
+
+# Repository
+from githubkit.versions.latest.models import (  # noqa: F401
+    Repository as RepositoryLatest,
+)
+from githubkit.versions.v2022_11_28.models import (  # noqa: F401
+    Repository as RepositoryV2022_11_28,
+)
+from githubkit.versions.ghec_v2022_11_28.models import (  # noqa: F401
+    Repository as RepositoryGhecV2022_11_28,
+)
+
+# isort: split
+
+# OrganizationFull
+from githubkit.versions.latest.models import (  # noqa: F401
+    OrganizationFull as OrganizationFullLatest,
+)
+from githubkit.versions.v2022_11_28.models import (  # noqa: F401
+    OrganizationFull as OrganizationFullV2022_11_28,
+)
+from githubkit.versions.ghec_v2022_11_28.models import (  # noqa: F401
+    OrganizationFull as OrganizationFullGhecV2022_11_28,
+)
+
+# isort: split
+
+# CustomProperty
+from githubkit.versions.latest.models import (  # noqa: F401
+    CustomProperty as CustomPropertyLatest,
+)
+from githubkit.versions.v2022_11_28.models import (  # noqa: F401
+    CustomProperty as CustomPropertyV2022_11_28,
+)
+from githubkit.versions.ghec_v2022_11_28.models import (  # noqa: F401
+    CustomProperty as CustomPropertyGhecV2022_11_28,
 )
