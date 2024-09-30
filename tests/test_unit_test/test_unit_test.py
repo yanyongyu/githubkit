b444dec20600a62572cf386499617d16e39886d9
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2024-09-30 14:56:54 +0800

    :memo: Docs: add unit test example (#142)

---
+++ tests/test_unit_test/test_unit_test.py
@@ -0,0 +1,78 @@
+import json
+from pathlib import Path
+from typing import Any, Type, Union, TypeVar
+
+import httpx
+import pytest
+
+from githubkit import GitHub
+from githubkit.utils import UNSET
+from githubkit.response import Response
+from githubkit.typing import URLTypes, UnsetType
+from githubkit.versions.latest.models import FullRepository
+
+T = TypeVar("T")
+
+
+FAKE_RESPONSE = json.loads((Path(__file__).parent / "fake_response.json").read_text())
+
+
+def target_sync_func():
+    github = GitHub("xxxxx")
+    resp = github.rest.repos.get("owner", "repo")
+    return resp.parsed_data
+
+
+def mock_request(
+    g: GitHub,
+    method: str,
+    url: URLTypes,
+    *,
+    response_model: Union[Type[T], UnsetType] = UNSET,
+    **kwargs: Any,
+) -> Response[Any]:
+    if method == "GET" and url == "/repos/owner/repo":
+        return Response[T](
+            httpx.Response(status_code=200, json=FAKE_RESPONSE),
+            Any if response_model is UNSET else response_model,
+        )
+    raise RuntimeError(f"Unexpected request: {method} {url}")
+
+
+def test_sync_mock():
+    with pytest.MonkeyPatch.context() as m:
+        m.setattr(GitHub, "request", mock_request)
+
+        repo = target_sync_func()
+        assert isinstance(repo, FullRepository)
+
+
+async def target_async_func():
+    github = GitHub("xxxxx")
+    resp = await github.rest.repos.async_get("owner", "repo")
+    return resp.parsed_data
+
+
+async def mock_arequest(
+    g: GitHub,
+    method: str,
+    url: URLTypes,
+    *,
+    response_model: Union[Type[T], UnsetType] = UNSET,
+    **kwargs: Any,
+) -> Response[Any]:
+    if method == "GET" and url == "/repos/owner/repo":
+        return Response[T](
+            httpx.Response(status_code=200, json=FAKE_RESPONSE),
+            Any if response_model is UNSET else response_model,  # type: ignore
+        )
+    raise RuntimeError(f"Unexpected request: {method} {url}")
+
+
+@pytest.mark.anyio
+async def test_async_mock():
+    with pytest.MonkeyPatch.context() as m:
+        m.setattr(GitHub, "arequest", mock_arequest)
+
+        repo = await target_async_func()
+        assert isinstance(repo, FullRepository)
